bc0683781b5a4bb8920032be3e7688c5fde592bb
amirhooseindaliri <amirhataeidaliri1378@gmail.com>
2021-09-22 19:05:59 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{msg}}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {observable} from './Rx'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      msg:''
+    }
+  },
+  mounted(){
+    observable.subscribe((res)=>{
+      this.msg = res
+    })
   }
 }
 </script>
---
+++ src/Rx.js
@@ -0,0 +1,15 @@
+import { Observable } from 'rxjs';
+export const observable = new Observable(subscriber => {
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+    }, 1000);
+});
+observable.subscribe({
+    next(x) { console.log('got value ' + x); },
+    error(err) { console.error('something wrong occurred: ' + err); },
+    complete() { console.log('done'); }
+})
